40c3eb92915788e8418292ddf003e234c97d872e
Appsody Devops <appsodydev@gmail.com>
2019-10-28 22:09:18 +0000

    Travis build: 0.4.9-109

--- appsody.rb
+++ appsody.rb
@@ -1,9 +1,9 @@
 class Appsody < Formula
   desc "The Appsody command-line interface"
   homepage "https://www.appsody.dev"
-  url "https://github.com/appsody/appsody/releases/download/0.2.5/appsody-homebrew-0.2.5.tar.gz"
-  # version "0.2.5"
-  sha256 "44b3f2c1de5e7ee30e0c648f59bdff56215fd123868511458fb5e26278a40788"
+  url "https://github.com/chilanti/appsody/releases/download/0.4.9/appsody-homebrew-0.4.9.tar.gz"
+  # version "0.4.9"
+  sha256 "51a36cb4e8b5bb82bfef07acb52e3eade3c28aca80c49666c720d6248f8ae784"
 
   def install
     bin.install "appsody"
